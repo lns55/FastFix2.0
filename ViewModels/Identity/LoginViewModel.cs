5255631855a20b79541567c7323af1ace9a659e9
lzn747 <lzn747@gmail.com>
2020-12-22 11:45:19 +0200

    Creating LoginViewModel.

---
+++ ViewModels/Identity/LoginViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace FastFix2._0.ViewModels.Identity
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [MaxLength(20)]
+        [Display(Name ="User Name")]
+        public string UserName { get; set; }
+
+        [Required]
+        [Display(Name ="Password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name ="Remember Me")]
+        public bool RememberMe { get; set; }
+
+        [HiddenInput(DisplayValue = false)]
+        public string ReturnUrl { get; set; }
+
+    }
+}
