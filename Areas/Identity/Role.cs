34dd5cfd74e8a6b4dff7493704637def7b25bf81
lzn747 <lzn747@gmail.com>
2020-12-24 17:24:23 +0200

    Creating Role Model and adding it to DB context.

---
+++ Areas/Identity/Role.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace FastFix2._0.Areas.Identity
+{
+    public class Role : IdentityRole
+    {
+        public const string Administrator = "Administrators";
+        public const string CarOwner = "Car Owners";
+        public const string CarRepair = "Car Repair Shops";
+        public string Description { get; set; }
+    }
+}
