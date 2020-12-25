adf24f6b07c6d56325bb3b33ead30c350c84fb49
lzn747 <lzn747@gmail.com>
2020-12-25 18:52:04 +0200

    Created ForgotPassword View.

---
+++ Models/Home/ResetPasswordModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FastFix2._0.Models.Home
+{
+    public class ResetPasswordModel
+    {
+        public string Token { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set;}
+    }
+}
