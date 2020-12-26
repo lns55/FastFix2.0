44ede0ffeb8681d3c5a0f254e610bd175352c592
lzn747 <lzn747@gmail.com>
2020-12-26 20:09:01 +0200

    Adding CarRepairUsers to DbContext.

--- Data/FastFixDbContext.cs
+++ Data/FastFixDbContext.cs
@@ -6,7 +6,7 @@ namespace FastFix2._0.Data
 {
     public class FastFixDbContext : IdentityDbContext<User, Role, string>
     {
-
+        public DbSet<CarRepairUser> carRepairUsers { get; set; }
         public FastFixDbContext(DbContextOptions<FastFixDbContext> options)
             : base(options)
         {
