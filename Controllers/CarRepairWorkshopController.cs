67244026e06687615e1b1fc135680f1a407c9709
lzn747 <lzn747@gmail.com>
2020-12-26 20:29:57 +0200

    Adding CarRepairWorkshopController.

---
+++ Controllers/CarRepairWorkshopController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FastFix2._0.Controllers
+{
+    public class CarRepairWorkshopController : Controller
+    {
+        public IActionResult CarRepairWorkshop() => View();
+    }
+}
