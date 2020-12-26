69dd503fcda63efc43c747535e8deaf8cd65c45c
lzn747 <lzn747@gmail.com>
2020-12-26 19:55:54 +0200

    Creating CarRepairDataRegistration View.

--- ViewModels/Identity/CarRepairDataRegistrationViewModel.cs
+++ ViewModels/Identity/CarRepairDataRegistrationViewModel.cs
@@ -4,26 +4,22 @@ namespace FastFix2._0.ViewModels.Identity
 {
     public class CarRepairDataRegistrationViewModel
     {
-        //Co is for Company
-
         [Required]
         [Display(Name ="Company Name")]
         public string CoName { get; set; }
 
         [Required]
-        [MaxLength(30)]
-        public string CoRegNumber { get; set; }
-
-        [Required]
+        [Display(Name ="Company Adress")]
         public string CoAdress { get; set; }
 
         [Required, MaxLength(15)]
+        [Display(Name ="Company Phone Number")]
         [DataType(DataType.PhoneNumber)]
         public string CoPhoneNumber { get; set; }
 
         [Required]
+        [Display(Name ="Company Email Adress")]
         [DataType(DataType.EmailAddress)]
         public string CoEmail { get; set; }
-
     }
 }
