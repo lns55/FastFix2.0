6f8ff6944ae7b274b7ea7e3bd97a458c596a5447
lzn747 <lzn747@gmail.com>
2020-12-30 21:05:32 +0200

    Creating Application Controller for CarOwners.

---
+++ Controllers/CarOwnerGarage/MyApplications/MyApplicationsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FastFix2._0.Controllers.CarOwnerGarage.MyApplications
+{
+    public class MyApplicationsController : Controller
+    {
+        public IActionResult New() => View();
+        public IActionResult Active() => View();
+        public IActionResult Waiting() => View();
+        public IActionResult Completed() => View();
+    }
+}
