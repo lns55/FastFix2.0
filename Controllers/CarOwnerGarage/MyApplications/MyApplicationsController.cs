dc16c428448e609098e386e8ab10689bc24a56a4
lzn747 <lzn747@gmail.com>
2021-01-02 18:57:56 +0200

    Creating NewApplications method.

--- Controllers/CarOwnerGarage/MyApplications/MyApplicationsController.cs
+++ Controllers/CarOwnerGarage/MyApplications/MyApplicationsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using FastFix2._0.ViewModels.Applications;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -9,7 +10,34 @@ namespace FastFix2._0.Controllers.CarOwnerGarage.MyApplications
 {
     public class MyApplicationsController : Controller
     {
-        public IActionResult New() => View();
+        /// <summary>
+        /// This method is resopnsible for creating new applications from CarOwners to CarReparis.
+        /// </summary>
+        public IActionResult New() => View(new CreateApplicationViewModel());
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> New(CreateApplicationViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            var app = new CreateApplicationViewModel
+            {
+                Id = model.Id,
+                IssueTitle = model.IssueTitle,
+                Car = model.Car,
+                RepairFrom = model.RepairFrom,
+                RepairTill = model.RepairTill,
+                City = model.City,
+                TypeOfWork = model.TypeOfWork,
+                Description = model.Description
+            };
+
+            if (model.Id == 0) 
+                
+        }
+
+
         public IActionResult Active() => View();
         public IActionResult Waiting() => View();
         public IActionResult Completed() => View();
