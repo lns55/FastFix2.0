a8f6bc3062811ca5076331903ada572562554262
lzn747 <lzn747@gmail.com>
2021-01-02 19:00:15 +0200

    Application Interface created.

---
+++ Infrastructure/Interfaces/IApp.cs
@@ -0,0 +1,18 @@
+﻿using FastFix2._0.ViewModels.Applications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FastFix2._0.Infrastructure.Interfaces
+{
+    public interface IApp
+    {
+        IEnumerable<CreateApplicationViewModel> Get();
+
+        CreateApplicationViewModel GetById(int Id);
+        int Add(CreateApplicationViewModel app);
+        bool Delete(int Id);
+        void SaveChanges();
+    }
+}
