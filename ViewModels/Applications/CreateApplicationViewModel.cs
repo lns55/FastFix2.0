060c711a69e8266c1fab77b0f1e4efe21b4f35d6
lzn747 <lzn747@gmail.com>
2021-01-02 16:51:06 +0200

    CreateApplication ViewModel created.

---
+++ ViewModels/Applications/CreateApplicationViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FastFix2._0.ViewModels.Applications
+{
+    public class CreateApplicationViewModel
+    {
+        public string IssueTitle { get; set; }
+        public string Car { get; set; }
+        public DateTime RepairFrom { get; set; }
+        public DateTime RepairTill { get; set; }
+        public string TypeOfWork { get; set; }
+        public string Description { get; set; }
+    }
+}
