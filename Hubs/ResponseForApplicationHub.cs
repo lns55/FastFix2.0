1d99f479d0e015bab2973a63e86ddd9f6efc33d7
lzn747 <lzn747@gmail.com>
2021-02-19 12:27:25 +0200

    Implementing SignalR Hub.

---
+++ Hubs/ResponseForApplicationHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace FastFix2._0.Hubs
+{
+    public class ResponseForApplicationHub : Hub
+    {
+        public async Task SendAnswer(string UserName, string Date, string Comment, int Price)
+        {
+            await Clients.All.SendAsync("ReceiveAnswer", UserName, Date, Comment, Price);
+        }
+    }
+}
