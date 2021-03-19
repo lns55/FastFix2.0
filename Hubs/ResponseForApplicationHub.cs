5bed5d0679f33dd43ce867bd591b4c2bc2221ae8
lzn747 <lzn747@gmail.com>
2021-03-19 16:21:30 +0200

    Completed fixing logic of diplaying new applications in CarRepairWorkshop.

--- Hubs/ResponseForApplicationHub.cs
+++ Hubs/ResponseForApplicationHub.cs
@@ -2,15 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FastFix2._0.Data;
 using Microsoft.AspNetCore.SignalR;
+
 
 namespace FastFix2._0.Hubs
 {
     public class ResponseForApplicationHub : Hub
     {
-        public async Task Send(string issue, string car, string date, string description)
+        private readonly FastFixDbContext _db;
+
+        public ResponseForApplicationHub(FastFixDbContext context)
         {
-            await Clients.All.SendAsync("Send", issue, car, date, description);
+            _db = context;
+        }
+
+        public async Task Send(string issue, string car , string date, string description)
+        {
+            var appId = from a in _db.NewApplications
+                        where a.Description == description
+                        select a.Id;
+
+            await Task.Delay(300);
+            await Clients.All.SendAsync("Send", issue, car, date, description, appId);
         }
     }
 }
