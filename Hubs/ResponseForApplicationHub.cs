a922e42e1508e5757ead3fc275e824751f33775f
lzn747 <lzn747@gmail.com>
2021-03-21 10:16:53 +0200

    Leaving for this moment the sorting new applications by city and type of work feature...cant find the working way....starting making logic of answer pages for new applications

--- Hubs/ResponseForApplicationHub.cs
+++ Hubs/ResponseForApplicationHub.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FastFix2._0.Areas.Identity;
 using FastFix2._0.Data;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.SignalR;
 
 
@@ -17,13 +19,20 @@ namespace FastFix2._0.Hubs
             _db = context;
         }
 
-        public async Task Send(string issue, string car , string date, string description)
+        public async Task Send(string issue, string car, string city, string date, string description)
         {
             var appId = from a in _db.NewApplications
                         where a.Description == description
                         select a.Id;
 
-            await Task.Delay(300);
+            //Need to do sorting by city and type of work. Leaving that tries here...hope you will find a better way...21.03.2021
+            //var sortByCity = from r in _db.carRepairUsers
+            //                 where r.City == city
+            //                 select r.UserId.ToString();
+
+            //var userID = sortByCity.ToList();
+
+            await Task.Delay(500);
             await Clients.All.SendAsync("Send", issue, car, date, description, appId);
         }
     }
