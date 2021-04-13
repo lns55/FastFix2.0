e5f591638e6afcb745ad3310c8c7ca8225143e16
lzn747 <lzn747@gmail.com>
2021-04-13 19:51:39 +0300

    CarOwner garage controller and views created + minor bug fixes in other views(CarOwner).

---
+++ Controllers/CarOwnerGarage/Garage/MyGarageController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FastFix2._0.Controllers.CarOwnerGarage.Garage
+{
+    public class MyGarageController : Controller
+    {
+        public IActionResult MyCars()
+        {
+            return View();
+        }
+
+        public IActionResult AddCar()
+        {
+            return View();
+        }
+    }
+}
